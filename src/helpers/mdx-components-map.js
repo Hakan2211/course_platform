3cb6af211b54f2ae4501ae893ea5086b954c3d3e
hakan <hbilgic1992@gmail.com>
2025-03-31 13:55:19 +0200

    create quotecomponent and add slider form shadcn

--- src/helpers/mdx-components-map.js
+++ src/helpers/mdx-components-map.js
@@ -1,9 +1,11 @@
 import BlogHeading from '@/components/mdx_components/blogHeading/blogHeading';
+import QuoteComponent from '@/components/mdx_components/quoteComponent/quoteComponent';
 
 const COMPONENT_MAP = {
   h1: (props) => <BlogHeading level={1} {...props} />,
   h2: (props) => <BlogHeading level={2} {...props} />,
   h3: (props) => <BlogHeading level={3} {...props} />,
+  QuoteComponent,
 };
 
 export default COMPONENT_MAP;
