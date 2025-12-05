309f2343fae9e3d351dbcdc0486eecec8db95aec
hakan <hbilgic1992@gmail.com>
2025-12-05 17:21:25 +0100

    change image hero section /course route and make curriculum for module 4

--- src/components/hero/NavBar.tsx
+++ src/components/hero/NavBar.tsx
@@ -5,12 +5,12 @@ export function NavBar() {
     <nav className="absolute top-0 left-0 w-full z-10 p-6 flex justify-between items-center pointer-events-none">
       <div className="pointer-events-auto">
         <Link href="/" className="text-white font-bold text-xl tracking-wider">
-          TRADERS<span className="text-blue-400">.EDU</span>
+          Logo<span className="text-blue-400"></span>
         </Link>
       </div>
       <div className="pointer-events-auto">
         <Link
-          href="/notes"
+          href="/course/notes"
           className="text-white/80 hover:text-white transition-colors border border-white/20 px-4 py-2 rounded-full backdrop-blur-sm bg-black/10"
         >
           My Notes
